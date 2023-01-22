b179b5bccd5ae75f99a6ef55d8f7ea978e108358
rakeshcooper <rakeshmurali097@gmail.com>
2023-01-22 21:05:12 +0530

    react-practice-updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  // write javascript code here
+let counter = 0 ;
+const incrementer = () => {
+  counter += 1;
+  console.log(counter);
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter {counter}</h1>
+      <button onClick={incrementer}>click</button>
     </div>
+    
   );
 }
 
